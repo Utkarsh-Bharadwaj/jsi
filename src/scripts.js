d04df895c9eb5c6294b8881bf6cb4f303a02102c
Utkarsh-Bharadwaj <UtkarshUpadhyay@cedcoss.com>
2022-02-05 14:00:07 +0530

    Hands_ON JAVASCRIPT

--- src/scripts.js
+++ src/scripts.js
@@ -272,5 +272,12 @@ const myArray = [
   
   const myData = myArray[2][1];
 
+/*task 44*/
+  // Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(["dog",3])
+
 
 
