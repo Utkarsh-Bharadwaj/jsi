3585e3323351f638ea4703c2cf0fdf3b33fca3db
Utkarsh-Bharadwaj <UtkarshUpadhyay@cedcoss.com>
2022-02-05 15:11:07 +0530

    Hands_ON JAVASCRIPT

--- src/scripts.js
+++ src/scripts.js
@@ -396,6 +396,19 @@ function addFive() {
 addThree();
 addFive();
 
+ /*task 56*/
+
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+processed = processArg(7);
+
+// Only change code below this line
+
 
 
 
