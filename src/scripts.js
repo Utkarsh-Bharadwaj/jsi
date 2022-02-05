a0083608a17c184c88c539a0ff7d890adf12cd39
Utkarsh-Bharadwaj <UtkarshUpadhyay@cedcoss.com>
2022-02-05 17:32:06 +0530

    hands on js

--- src/scripts.js
+++ src/scripts.js
@@ -1068,6 +1068,30 @@ phoneticLookup("charlie");
     
     }
 
+         /*task 92*/
+
+         var myMusic = [
+          {
+            "artist": "Billy Joel",
+            "title": "Piano Man",
+            "release_year": 1973,
+            "formats": [
+              "CS",
+              "8T",
+              "LP" ],
+            "gold": true
+          },
+          // Add record here
+          {
+            "artist": "Bob",
+            "title": "Hello",
+            "release_year": 1950,
+            "formats": [
+              "CS",
+              "8T"
+            ]
+          }
+        ];
 
 
 
