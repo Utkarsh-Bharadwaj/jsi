b143116215935c9c5adf6d5b20142eddb8e56e12
Utkarsh-Bharadwaj <UtkarshUpadhyay@cedcoss.com>
2022-02-05 16:34:30 +0530

    hands on js

--- src/scripts.js
+++ src/scripts.js
@@ -713,6 +713,35 @@ function compareEquality(a, b) {
       // Change these values to test
       golfScore(5, 1); 
 
+        /*task 76*/
+
+      function caseInSwitch(val) {
+        var answer = "";
+        // Only change code below this line
+      
+        switch (val) {
+          case 1:
+            answer = "alpha";
+            break;
+          case 2:
+            answer = "beta";
+            break;
+          case 3:
+            answer = "gamma";
+            break;
+          case 4:
+            answer = "delta";
+            break;
+        }
+      
+      
+        // Only change code above this line
+        return answer;
+      }
+      
+      // Change this value to test
+      caseInSwitch(1);
+
 
 
 
