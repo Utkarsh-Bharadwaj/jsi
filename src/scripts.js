a14a651f402950d2803918e4206d33fd098cf24c
Utkarsh-Bharadwaj <UtkarshUpadhyay@cedcoss.com>
2022-02-05 17:10:39 +0530

    hands on js

--- src/scripts.js
+++ src/scripts.js
@@ -1005,6 +1005,31 @@ const myDog = {
 
 myDog.bark="wolf"
 
+/*task 89*/
+
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"],
+  "bark": "bow-wow"
+};
+
+delete ourDog.bark;
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Free Code Camp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line.
+
+delete myDog.tails;
+
 
 
 
