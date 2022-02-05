8dd4041e99b2efe4a6af29298a4f79a98808d409
Utkarsh-Bharadwaj <UtkarshUpadhyay@cedcoss.com>
2022-02-05 17:02:41 +0530

    hands on js

--- src/scripts.js
+++ src/scripts.js
@@ -970,6 +970,31 @@ var playerNumber = 16;       // Change this Line
 var player = testObj[playerNumber];   // Change this Line
 
 
+  /*task 87*/
+
+  // Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Free Code Camp Campers"]
+};
+
+// Only change code below this line.
+
+myDog.name = "Happy Coder"; // Dot notation
+myDog["name"] = "Happy Coder"; // Bracket notation
+
 
 
 
