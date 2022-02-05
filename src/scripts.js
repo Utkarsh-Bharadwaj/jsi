6bb5b0c1fbe8081d2598e48b60a32e4ed48f88e7
Utkarsh-Bharadwaj <UtkarshUpadhyay@cedcoss.com>
2022-02-05 16:38:58 +0530

    hands on js

--- src/scripts.js
+++ src/scripts.js
@@ -766,9 +766,41 @@ function compareEquality(a, b) {
         // Only change code above this line
         return answer;
       }
+
+        /*task 78*/
       
       // Change this value to test
       switchOfStuff(1);
+
+      function sequentialSizes(val) {
+        var answer = "";
+        // Only change code below this line
+      
+        switch (val) {
+          case 1:
+          case 2:
+          case 3:
+            answer = "Low";
+            break;
+          case 4:
+          case 5:
+          case 6:
+            answer = "Mid";
+            break;
+          case 7:
+          case 8:
+          case 9:
+            answer = "High";
+            break;
+        }
+      
+      
+        // Only change code above this line
+        return answer;
+      }
+      
+      // Change this value to test
+      sequentialSizes(1);
 
 
 
