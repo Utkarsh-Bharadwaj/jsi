2b31470a37e19fed0ce9c415fc332f0d173238eb
Utkarsh-Bharadwaj <UtkarshUpadhyay@cedcoss.com>
2022-02-05 16:36:53 +0530

    hands on js

--- src/scripts.js
+++ src/scripts.js
@@ -742,6 +742,34 @@ function compareEquality(a, b) {
       // Change this value to test
       caseInSwitch(1);
 
+      /*task 77*/
+
+      function switchOfStuff(val) {
+        var answer = "";
+        // Only change code below this line
+      
+        switch (val) {
+          case "a":
+            answer = "apple";
+            break;
+          case "b":
+            answer = "bird";
+            break;
+          case "c":
+            answer = "cat";
+            break;
+          default:
+            answer = "stuff";
+        }
+      
+      
+        // Only change code above this line
+        return answer;
+      }
+      
+      // Change this value to test
+      switchOfStuff(1);
+
 
 
 
