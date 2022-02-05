b85c26200e34a72e27cec7bb09798d9d9285fc27
Utkarsh-Bharadwaj <UtkarshUpadhyay@cedcoss.com>
2022-02-05 15:58:11 +0530

    hands on js

--- src/scripts.js
+++ src/scripts.js
@@ -500,6 +500,24 @@ function compareEquality(a, b) {
   // Change this value to test
   testNotEqual(10);
 
+  /*task 64*/
+
+  function testStrictNotEqual(val) {
+    // Only Change Code Below this Line
+  
+    if (val !== 17) {
+  
+    // Only Change Code Above this Line
+  
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  // Change this value to test
+  testStrictNotEqual(10);
+
+
 
 
 
