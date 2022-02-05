f71dd83fbf6805ddbdf6d88b7f80deb4a184c931
Utkarsh-Bharadwaj <UtkarshUpadhyay@cedcoss.com>
2022-02-05 17:36:51 +0530

    hands on js

--- src/scripts.js
+++ src/scripts.js
@@ -1113,6 +1113,29 @@ phoneticLookup("charlie");
 
          /*task 94*/
 
+         var myPlants = [
+          {
+            type: "flowers",
+            list: [
+              "rose",
+              "tulip",
+              "dandelion"
+            ]
+          },
+          {
+            type: "trees",
+            list: [
+              "fir",
+              "pine",
+              "birch"
+            ]
+          }
+        ];
+        
+        // Only change code below this line
+        
+        var secondTree = myPlants[1].list[1]; // Change this line
+
 
 
 
