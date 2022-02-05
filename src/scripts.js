b05f1a4b2cba641b77fde8af6339b1e513883eac
Utkarsh-Bharadwaj <UtkarshUpadhyay@cedcoss.com>
2022-02-05 13:33:00 +0530

    Hands_ON JAVASCRIPT

--- src/scripts.js
+++ src/scripts.js
@@ -201,3 +201,12 @@ let myStr = "Jello World";
 // Only change code below this line
 myStr = "Hello World"; // Change this line
 // Only change code above this line
+
+/*task 35*/
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+
+const thirdLetterOfLastName = lastName[2]; // Change this line
