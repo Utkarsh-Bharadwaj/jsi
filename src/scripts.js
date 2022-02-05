b2573ae73d051a9ac97647565bd0f00af58bd949
Utkarsh-Bharadwaj <UtkarshUpadhyay@cedcoss.com>
2022-02-05 16:09:07 +0530

    hands on js

--- src/scripts.js
+++ src/scripts.js
@@ -552,6 +552,23 @@ function compareEquality(a, b) {
   // Change this value to test
   testGreaterThan(10);
 
+ /*task 67*/
+ 
+  function testLessThan(val) {
+    if (val < 25) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val < 55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  // Change this value to test
+  testLessThan(10);
+
 
 
 
