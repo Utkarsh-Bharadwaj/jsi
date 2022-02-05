9744de53c923ad69cdb46d43d51c25f0ba6c26c3
Utkarsh-Bharadwaj <UtkarshUpadhyay@cedcoss.com>
2022-02-05 17:00:09 +0530

    hands on js

--- src/scripts.js
+++ src/scripts.js
@@ -955,6 +955,21 @@ var entreeValue = testObj["an entree"];   // Change this line
 var drinkValue = testObj["the drink"];    // Change this line
 
 
+/*task 86*/
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
+
+
 
 
 
