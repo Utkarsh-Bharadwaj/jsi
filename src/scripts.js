a4f3c56c2929b2e5bbad8a06f7c59eea9de1a2cd
Utkarsh-Bharadwaj <UtkarshUpadhyay@cedcoss.com>
2022-02-05 11:44:57 +0530

    Hands_on_javascript

--- src/scripts.js
+++ src/scripts.js
@@ -34,3 +34,10 @@ titleCaseOver = 9000;
 
 let catName = "Oliver";
 let catSound = "Meow!";
+
+/* task 10*/
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
