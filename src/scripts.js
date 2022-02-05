69c22d6a1ddf9ddb3e25d069a2a9083cd87cde6a
Utkarsh-Bharadwaj <UtkarshUpadhyay@cedcoss.com>
2022-02-05 15:30:14 +0530

    Hands_ON JAVASCRIPT

--- src/scripts.js
+++ src/scripts.js
@@ -440,6 +440,19 @@ function nextInLine(arr, item) {
     }
 
 
+  /*task 59*/
+
+  function trueOrFalse(wasThatTrue) {
+
+    // Only change code below this line.
+    if(wasThatTrue) {
+      return "Yes, that was true";
+    }
+    return "No, that was false";
+  
+    // Only change code above this line.
+  
+  }
 
 
 
