9cf0609eb3de9c6d598afbdf64180b9cc9c110d7
Utkarsh-Bharadwaj <UtkarshUpadhyay@cedcoss.com>
2022-02-05 18:36:30 +0530

    hands on js

--- src/scripts.js
+++ src/scripts.js
@@ -1199,7 +1199,42 @@ updateRecords(recordCollection, 5439, 'artist', 'ABBA');
          myArray.push(i);
 
 }
+
+      /*task 98*/
     // Only change code below this line
+
+    // Setup
+       const myArray = [];
+    for (let i = 1; i < 10; i += 2) {
+         myArray.push(i);
+    // Only change code below this line
+     }
+
+
+      /*task 99*/
+     // Setup
+const myArray = [];
+for (let i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
+// Only change code below this line
+
+   /*task 100*/
+   
+   // Setup
+const myArr = [2, 3, 4, 5, 6];
+
+
+
+let total=0;
+for (let i = 0; i < (myArr.length); i++) {
+
+
+total+=myArr[i];
+
+}
+
+// Only change code below this line
 
 
 
